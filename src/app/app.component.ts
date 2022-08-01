c51448081638df0d2c83ad58c67d13965ee68949
Thibaut <tibeloute@msn.com>
2022-08-02 01:25:22 +0200

    part 6 : placer la liste dans app.component pour la suite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProjectTp2';
+  articles: Article[];
+
+constructor() {
+  this.articles = [
+    new Article('truc','https://www.codetogether.com/',15),
+    new Article('machin','https://www.bitechworld.com/fr/base-de-donn%C3%A9es-sql/',7),
+    new Article('chouette','https://screensiz.es/tablet',54),
+    new Article('mono','https://www.similartech.com/api',20),
+  ];
+}
+
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement) {
     console.log(`Adding article title: ${title.value} and link: ${link.value } `);
     return false;
   }
+
+
 }
 
