c9522396b2e160dcef33543294f12eaa8a77b7a6
Thibaut <tibeloute@msn.com>
2022-08-02 01:35:32 +0200

    Part 9 : Ajout d'une fonction en fat arrow pour le trie en fonction du nombres de vote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,24 +10,27 @@ export class AppComponent {
   title = 'ProjectTp2';
   articles: Article[];
 
-constructor() {
-  this.articles = [
-    new Article('truc','https://www.codetogether.com/',15),
-    new Article('machin','https://www.bitechworld.com/fr/base-de-donn%C3%A9es-sql/',7),
-    new Article('chouette','https://screensiz.es/tablet',54),
-    new Article('mono','https://www.similartech.com/api',20),
-  ];
-}
+  constructor() {
+    this.articles = [
+      new Article('truc', 'https://www.codetogether.com/', 15),
+      new Article('machin', 'https://www.bitechworld.com/fr/base-de-donn%C3%A9es-sql/', 7),
+      new Article('chouette', 'https://screensiz.es/tablet', 54),
+      new Article('mono', 'https://www.similartech.com/api', 20),
+    ];
+  }
 
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement) {
-    console.log(`Adding article title: ${title.value} and link: ${link.value } `);
-    this.articles.push(new Article(title.value, link.value,0));
+    console.log(`Adding article title: ${title.value} and link: ${link.value} `);
+    this.articles.push(new Article(title.value, link.value, 0));
     title.value = ''; // reset values
     title.value = '';
     return false;
   }
 
+  sortedArticle(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 
 }
 
